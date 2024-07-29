ed9394b4a226caa1245b4e8d4135c20a7c6bbbb6
Salsashoupizahra <141709266+Salsashoupizahra@users.noreply.github.com>
2024-07-29 10:58:49 +0700

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="row bg-primary text-light align-items-start ">
+      <div class="col-lg-2">
+        <img src="~/assets/img/logoo.png"
+        alt=""
+        width="80"
+        height="80"
+        class="d-inline-block align-text-top left-20">
+      </div>
+      <div class="col-lg">
+        <h1>BUKU PENGUNJUNG PERPUSTAKAAN</h1>
+        <P>
+          JL.DEPOK,SUKAMENAK,KEC.PURBARATU,KOTA.TASIKMALAYA,JAWA BARAT
+        </P>
+      </div>
+    </div>
+    <slot />
+</div>
+</template>
